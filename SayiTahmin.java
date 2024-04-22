71327b6fa81231c266e145205fedd944529fafa9
senaince <157987013+senaince@users.noreply.github.com>
2024-04-22 09:18:06 +0300

    SayiTahmin.java

--- SayiTahmin.java
+++ SayiTahmin.java
@@ -6,19 +6,19 @@ public class SayiTahmin {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        int rastgeleSayi = random.nextInt(10) + 1; // 1 ile 10 arasında rastgele bir sayı üretir
+        int rastgeleSayi = random.nextInt(10) + 1; 
         int tahmin;
 
         System.out.println("1 ile 10 arasında bir sayı tahmin edin.");
 
-        // Kullanıcı doğru tahmin edene kadar devam eden döngü
+       
         while (true) {
             System.out.print("Tahmininiz: ");
             tahmin = scanner.nextInt();
 
             if (tahmin == rastgeleSayi) {
                 System.out.println("Tebrikler! Doğru tahmin ettiniz.");
-                break; // Doğru tahmin edildiği için döngüden çıkılır
+                break; 
             } else {
                 System.out.println("Yanlış tahmin ettiniz. Tekrar deneyin.");
             }
